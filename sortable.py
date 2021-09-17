2836c5bc00f596327f43c218362fd529a769134d
Sayo Everden <syeverden@gmail.com>
2021-09-17 05:02:54 -0700

    adding String list

--- sortable.py
+++ sortable.py
@@ -2,5 +2,8 @@ sortable = [4,"s",3,"a",7, 9,"y","o"]
 print(sortable)
 
 int_list = [x for x in sortable if isinstance(x,int)]
+String_list = [x for x in sortable if isinstance(x,str)]
 int_list.sort()
+String_list.sort()
 print(int_list)
+print(String_list)
