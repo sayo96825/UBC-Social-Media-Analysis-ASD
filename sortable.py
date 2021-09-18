c59911478168e6f2e34419d1a3d7e3ba0d57aa4b
Sayo Everden <syeverden@gmail.com>
2021-09-18 10:47:11 -0700

    making changes

--- sortable.py
+++ sortable.py
@@ -1,2 +1,7 @@
 sortable = [4,"s",3,"a",7, 9,"y","o"]
 print(sortable)
+#int_list = [x for x in sortable if isinstance(x, int)]
+#String_list = [x for x in sortable if isinstance(x, str)]
+#print(int_list)
+#print(String_list)
+#making changes
